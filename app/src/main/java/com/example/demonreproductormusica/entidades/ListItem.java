2901c5959b6509096aa225ff9a1fe098584ef343
jcarloshg <carlosj12336@gmail.com>
2021-10-09 18:37:31 -0500

    ADD state to ListItem && his get/set methods

--- app/src/main/java/com/example/demonreproductormusica/entidades/ListItem.java
+++ app/src/main/java/com/example/demonreproductormusica/entidades/ListItem.java
@@ -2,14 +2,21 @@ package com.example.demonreproductormusica.entidades;
 
 public class ListItem {
 
+    public static final String ITEM_PLAYLIST_VIEW = "ITEM_PLAYLIST_VIEW";
+    public static final String ITEM_PLAYLIST_LIST = "ITEM_PLAYLIST_VIEW";
+    public static final String ITEM_SONG_VIEW = "ITEM_SONG_VIEW";
+    public static final String ITEM_SONG_LIST = "ITEM_SONG_LIST";
+
     private int id;
     private String title;
     private String subtitle;
+    private String TYPE;
 
-    public ListItem(int _id, String _title, String _subtitle){
+    public ListItem(int _id, String _title, String _subtitle, String _TYPE) {
         this.id = _id;
         this.title = _title;
         this.subtitle = _subtitle;
+        this.TYPE = _TYPE;
     }
 
     public ListItem() {
@@ -37,5 +44,13 @@ public class ListItem {
 
     public void setSubtitle(String subtitle) {
         this.subtitle = subtitle;
+    }
+
+    public String getTYPE() {
+        return TYPE;
+    }
+
+    public void setTYPE(final String type) {
+        this.TYPE = type;
     }
 }
